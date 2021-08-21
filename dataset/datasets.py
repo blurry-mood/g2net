d16bcab18f0d62f92e8f30ab70abcd0b97f68d10
blurry-mood <assis.ayoub@gmail.com>
2021-08-21 20:20:55 +0100

    Add Specadtaset  & TransformDataset

---
+++ dataset/datasets.py
@@ -0,0 +1,32 @@
+import numpy as np
+from torch.utils.data import Dataset
+import torch
+
+class TransformDataset(Dataset):
+
+    def __init__(self, paths, transform):
+        super().__init__()
+        self.paths = paths
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, i):
+        ts = np.load(self.paths[i]).astype('float32')
+        ts = torch.from_numpy(ts)
+        specs = self.transform(ts)
+        return i, specs
+
+class SpecDataset(Dataset):
+
+    def __init__(self, paths):
+        super().__init__()
+        self.paths = paths
+
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, i):
+        ts = np.load(self.paths[i]).astype('float32')
+        return i, ts
