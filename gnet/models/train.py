6212993dc44d19a6d60470787a1bc54163b7c337
blurry-mood <assis.ayoub@gmail.com>
2021-08-22 10:46:35 +0100

    remove argparser from train.py

--- gnet/models/train.py
+++ gnet/models/train.py
@@ -9,28 +9,31 @@ from .litmodel import LitModel
 import os
 
 # arguments
-parser = argparse.ArgumentParser(description='Train Lightning Module.')
-parser.add_argument('--yaml', type=str, required=True,
-                    help='YAML config file containing the lit model description.')
-parser.add_argument('--data', type=str, required=True,
-                    help='Path to training dataset. The training folder should contain train_labels.csv with keys [id, target]. Wildcards are supported.')
-args = parser.parse_args()
+# parser = argparse.ArgumentParser(description='Train Lightning Module.')
+# parser.add_argument('--yaml', type=str, required=True,
+#                     help='YAML config file containing the lit model description.')
+# parser.add_argument('--data', type=str, required=True,
+#                     help='Path to training dataset. The training folder should contain train_labels.csv with keys [id, target]. Wildcards are supported.')
+# args = parser.parse_args()
 
-cfg = os.path.join(os.path.split(__file__)[0], 'config', args.yaml)
-data_path = args.data
+# cfg = os.path.join(os.path.split(__file__)[0], 'config', args.yaml)
+# data_path = args.data
 
-# model & datamodule
-litmodel = LitModel(cfg)
-dm = DataModule(data_path, cfg.datamodule, cfg.split)
+def train(cfg_name, data_path):
+    cfg = os.path.join(os.path.split(__file__)[0], 'config', cfg_name)
 
-# wandb logger & lr monitor
-wandb.login()
-logger = WandbLogger(entity='blurry-mood', project='g2net')
-lr_monitor = LearningRateMonitor(logging_interval='epoch')
+    # model & datamodule
+    litmodel = LitModel(cfg)
+    dm = DataModule(data_path, cfg.datamodule, cfg.split)
 
-# trainer
-trainer = Trainer(**dict(cfg.trainer), callbacks=[lr_monitor], logger=logger)
+    # wandb logger & lr monitor
+    wandb.login()
+    logger = WandbLogger(entity='blurry-mood', project='g2net')
+    lr_monitor = LearningRateMonitor(logging_interval='epoch')
 
-# Fit and test
-trainer.fit(litmodel, dm)
-trainer.test(litmodel)
+    # trainer
+    trainer = Trainer(**dict(cfg.trainer), callbacks=[lr_monitor], logger=logger)
+
+    # Fit and test
+    trainer.fit(litmodel, dm)
+    trainer.test(litmodel)
