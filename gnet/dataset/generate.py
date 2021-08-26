162b6a355b7851dcf4fd5b28cab3ea2de8bb96a5
blurry-mood <assis.ayoub@gmail.com>
2021-08-26 18:32:14 +0100

    change config

--- gnet/dataset/generate.py
+++ gnet/dataset/generate.py
@@ -9,10 +9,63 @@ import numpy as np
 from torch.utils.data import DataLoader
 
 
-from .datasets import TransformDataset
-from .transforms import get_nnaudio_transform
-from ..utils import get_logger
+from datasets import TransformDataset
+from transforms import get_nnaudio_transform
+from logging import DEBUG, INFO, getLogger, Formatter, StreamHandler
 
+__all__ = ['get_logger']
+
+def get_logger():
+    logger = getLogger('G2Net')
+    logger.setLevel(DEBUG)
+
+    # formatter
+
+    # stream handler
+    if not logger.hasHandlers():
+        fmr = _ColoredFormatter('%(name)s: %(filename)s:%(lineno)s - %(levelname)s:  %(message)s')
+        ch = StreamHandler()
+        ch.setLevel(DEBUG)
+        ch.setFormatter(fmr)
+
+        logger.addHandler(ch)
+    
+    return logger
+
+
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+#The background is set with 40 plus the number of the color, and the foreground with 30
+
+#These are the sequences need to get colored ouput
+RESET_SEQ = "\033[0m"
+COLOR_SEQ = "\033[;%dm"
+BOLD_COLOR_SEQ = "\033[1;%dm"
+
+_COLORS = {
+    'WARNING': YELLOW,
+    'INFO': GREEN,
+    'DEBUG': BLUE,
+    'CRITICAL': YELLOW,
+    'ERROR': RED
+}
+
+class _ColoredFormatter(Formatter):
+    def __init__(self, msg, use_color = True):
+        Formatter.__init__(self, msg)
+        self.use_color = use_color
+
+    def format(self, record):
+        levelname = record.levelname
+        if self.use_color and levelname in _COLORS:
+            levelname_color = COLOR_SEQ % (30 + _COLORS[levelname]) + levelname + RESET_SEQ
+            record.levelname = levelname_color
+
+        # name
+        name = record.name
+        if self.use_color:
+            name_color = BOLD_COLOR_SEQ % (30 + RED) + name + RESET_SEQ
+            record.name = name_color
+        return Formatter.format(self, record)
 
 _logger = get_logger()
 
@@ -75,11 +128,11 @@ def _preprocess(yml_path, dataset_path, output_path):
             # normalize
             spec = (specs[i] - mean) / std
             # stack along this axis: `stacking`
-            spec = np.concatenate([spec[_i] for _i in range(3)], axis=stacking)
+            spec = np.concatenate([spec[_i][None] for _i in range(3)], axis=stacking)
 
             if log_size:
                 log_size = False
-                _logger.info(f'The new input shape: {spec.shape}')
+                _logger.info(f'The old shape: {specs[i].shape}, the new input shape: {spec.shape}')
 
             # save
             np.save(name, spec)
@@ -93,3 +146,6 @@ def stft(in_path, out_path, ):
 
 def mel(in_path, out_path, ):
     _preprocess(os.path.join(_CONFIGS, 'mel.yaml'), in_path, out_path, )
+
+
+stft('data', 'data2')
