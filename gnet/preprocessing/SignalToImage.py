8ff2fdb82b916f5d092d25d1b69b96ed95b9ab7d
blurry-mood <assis.ayoub@gmail.com>
2021-09-19 21:33:58 +0100

    convert stride to tuple

--- gnet/preprocessing/SignalToImage.py
+++ gnet/preprocessing/SignalToImage.py
@@ -14,6 +14,7 @@ class SignalToImage(nn.Module):
 
         mods = []
         for i in range(n):
+            cfg[i].stride = tuple(cfg[i].stride)
             mods.append(nn.Conv2d(**dict(cfg[i])))
             mods.append(nn.SiLU())
 
