0223dbe8e22986d268056c566dfd304cee8fc6ea
blurry-mood <assis.ayoub@gmail.com>
2021-09-22 12:56:59 +0100

    todo: compare b0 & paper w/ bceloss & aucmarginloss trained for 8 epochs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from gnet.model.train import *
 
 
-train('paper', 'stft', 'small', 'data')
+train('b0_bce', 'stft', 'small', 'data')
 # successive_train('paper', 'paper_wd', pre_cfg_name='stft', dm_cfg_name='small', data_path='data')
