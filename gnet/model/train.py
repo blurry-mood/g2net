a08712e7774f9158e2330a28629a1584c00db4b8
blurry-mood <assis.ayoub@gmail.com>
2021-09-23 09:45:30 +0100

    log artifacts to wandb at the end

--- gnet/model/train.py
+++ gnet/model/train.py
@@ -39,10 +39,8 @@ def train(model_cfg_name, pre_cfg_name, dm_cfg_name, data_path):
         logger=logger
         )
     
-    # Fit
+    # Fit & test
     trainer.fit(litmodel, dm)
-
-    # Test
     trainer.test(litmodel)
     
     # clean output folder, then save only the ckpt file
@@ -51,67 +49,19 @@ def train(model_cfg_name, pre_cfg_name, dm_cfg_name, data_path):
         _logger.info('Cleaning output folder...')
         os.system('rm * -rf')
     trainer.save_checkpoint("litmodel.ckpt")
+
+    wandb.finish(0)
+
+    run = wandb.init(job_type="dataset-creation",  reinit=True)
 
     # log artifacts
-    # run = trainer.logger.experiment
-    # artifact = wandb.Artifact('my-model', type='model')
-    # artifact.add_file('litmodel.ckpt')
-    # run.log_artifact(artifact)
+    run = trainer.logger.experiment
+    artifact = wandb.Artifact('my-model', type='model')
+    artifact.add_file('litmodel.ckpt')
+    run.log_artifact(artifact)
     
     # artifact = wandb.Artifact('my-code', type='dataset')
-    # artifact.add_dir(os.path.join(_HERE, '..'))
-    # run.log_artifact(artifact)
+    artifact.add_dir(os.path.join(_HERE, '..'))
+    run.log_artifact(artifact)
 
-
-def successive_train(*model_cfg_names, pre_cfg_name, dm_cfg_name, data_path):
-    dm = DataModule(data_path, dm_cfg_name)
-
-    def configuration(path, second:bool=False):
-        
-        cfg = glob(os.path.join(_HERE, 'config', '**', path+'.yaml'), recursive=True)
-        cfg = OmegaConf.load(cfg[0])
-        
-        # model & datamodule
-        if second:
-            litmodel = _LITMODELS[cfg.type].load_from_checkpoint('litmodel.ckpt', config=cfg, preprocess_config_name=pre_cfg_name)
-        else:
-            litmodel = _LITMODELS[cfg.type](cfg, pre_cfg_name)
-
-        # wandb logger & lr monitor
-        logger = WandbLogger(entity='blurry-mood', project='g2net')
-        lr_monitor = LearningRateMonitor(logging_interval='epoch')
-
-        # trainer
-        trainer = Trainer(
-            gpus=-1 if torch.cuda.is_available() else 0,
-            **dict(cfg.trainer),
-            callbacks=[lr_monitor], 
-            logger=logger
-            )
-        
-        return trainer, litmodel
-        
-    """ First round"""
-    trainer, litmodel = configuration(model_cfg_names[0])
-    trainer.fit(litmodel, dm)
-    trainer.test(litmodel)
-    
-    # clean output folder, then save only the ckpt file
-    dirs = glob('*')
-    if not 'gnet' in dirs:      # to avoid executing this locally
-        _logger.info('Cleaning output folder...')
-        os.system('rm * -rf')
-    trainer.save_checkpoint("litmodel.ckpt")
-
-    """ Second round"""
-    for i in range(1, len(model_cfg_names)):
-        trainer, litmodel =  configuration(model_cfg_names[i], True)
-        trainer.fit(litmodel, dm)
-        trainer.test(litmodel)
-
-        # clean output folder, then save only the ckpt file
-        dirs = glob('*')
-        if not 'gnet' in dirs:      # to avoid executing this locally
-            _logger.info('Cleaning output folder...')
-            os.system('rm * -rf')
-        trainer.save_checkpoint("litmodel.ckpt")
+    wandb.finish(0)
